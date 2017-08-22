2c6cfbaae6da7ccdd92624ff5d2c4e7ec7a63ad0
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-08-22 10:45:42 -0400

    Make signup/login functional

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,6 @@ import * as APIUtil from '../util/session_api_util';
 import { receieveErrors } from './error_actions';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
 export const login = (user) => dispatch => {
   return APIUtil.login(user).then(
--- frontend/reducers/error_reducer.js
+++ frontend/reducers/error_reducer.js
@@ -1,10 +1,13 @@
 import { merge } from 'lodash';
-import { RECEIVE_ERRORS } from '../actions/session_actions';
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_ERRORS } from '../actions/error_actions';
 
 export default (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, state, { login: null }, { signup: null }, { logout: null });
     case RECEIVE_ERRORS:
       return merge({}, state, { [action.errorType]: action.errors.responseJSON });
     default:
