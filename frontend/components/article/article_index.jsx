97ebb4d8eaa6e1ebae45bb65a341dd7e0cdaa191
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-08-26 16:56:52 -0400

    Setup lifecycle methods to refresh articles every new stock show page

--- frontend/components/article/article_index.jsx
+++ frontend/components/article/article_index.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-const ArticleIndex = (props) => {
-  return (
-    <article>
-      greed is good
-    </article>
-  );
+class ArticleIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchArticles(this.props.ticker);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.ticker !== this.props.ticker) {
+      this.props.fetchArticles(newProps.ticker);
+    }
+  }
+
+  render() {
+    const allArticles = Object.values(this.props.articles).map((article, idx) => {
+      return <li key={ idx } >
+        <p>{article.title}</p>
+        <p>{article.summary}</p>
+        <p>{article.body}</p>
+      </li>
+    });
+
+    return (
+      <section id="article-index-section" >
+        <ul>{ allArticles }</ul>
+      </section>
+    );
+  }
 };
 
 export default ArticleIndex;
